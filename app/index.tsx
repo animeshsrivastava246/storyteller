d8ca7d51a6ef2a0636dbb68ea23387ce184d9067
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-12-10 22:19:41 +0530

    Add: NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import "./global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-2xl font-bold text-blue-600">
+        NativeWind works great with Expo!
+      </Text>
+    </View>
+  );
+}
