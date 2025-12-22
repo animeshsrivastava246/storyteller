932483912b7dab02892c372ca048c47e2f30bb90
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2025-12-22 15:39:42 +0530

    WIP: Adapting Liquid glass navigation - unstable
    Todo: Complete all the mandatory tasks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,30 @@
-import { Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+// import { Slot } from "expo-router";
+// import { StatusBar } from "expo-status-bar";
 
-export default function RootLayout() {
+// export default function RootLayout() {
+//   return (
+//     <>
+//       <StatusBar
+//         style="auto"
+//         translucent
+//       />
+//       <Slot />
+//     </>
+//   );
+// }
+import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+
+export default function TabLayout() {
   return (
-    <>
-      <StatusBar
-        style="auto"
-        translucent
-      />
-      <Slot />
-    </>
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" drawable="ic_menu_mylocation" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="story">
+        <Icon sf="gear" drawable="ic_menu_mylocation" />
+        <Label>Story</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
